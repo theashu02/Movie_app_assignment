ec996db4458586a73a967b68510130a6ff90e024
theashu02 <ashutoshchauhan1919@gmail.com>
2025-01-16 04:36:58 +0530

    fix bugs search bar but pagination in api is not fix further make api for pagiantion

--- src/Pages/SearchBar.tsx
+++ src/Pages/SearchBar.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { RxCross2 } from "react-icons/rx";
 
 interface SearchBarProps {
   onSearch: (query: string) => void;
@@ -12,21 +14,20 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newQuery = e.target.value;
     setQuery(newQuery);
-    onSearch(newQuery);
+    onSearch(`"${newQuery}"`); // Send query in double quotes
   };
 
-  // const handleClear = () => {
-  //   setQuery("");
-  //   onSearch("");
-  // };
+  const handleClear = () => {
+    setQuery("");
+    onSearch(""); // Clear search
+  };
 
   return (
     <div className="max-w-2xl mx-auto px-4 w-full">
       <div
         className={`
-          relative flex items-center w-full overflow-hidden
-          bg-white rounded-lg shadow-md transition-all duration-200
-          ${isFocused ? "ring-2 ring-blue-500 shadow-lg" : "hover:shadow-lg"}
+          relative flex gap-4 items-center w-full overflow-hidden
+          bg-white dark:bg-[#1c1d25] rounded-lg shadow-md transition-all duration-200
         `}
       >
         {/* Search Input */}
@@ -37,12 +38,17 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
           onChange={handleInputChange}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
-          className="
+          className={`
             w-full px-4 py-3 text-gray-700 dark:bg-[#2a2b35] dark:text-[#e0e0e0] dark:placeholder-[#6b6b6b]
-            placeholder-gray-400 bg-transparent dark:border-[#3a3b45] dark:focus:border-[#4a4b55] dark:focus:bg-[#3a3b45]
-            focus:outline-none
-          "
+            placeholder-gray-400 bg-transparent dark:border-[#3a3b45] 
+            ${isFocused ? "dark:border-[#4a4b55]" : "dark:border-[#3a3b45]"} 
+            dark:focus:bg-[#3a3b45] focus:outline-none
+          `}
         />
+        <Button variant="outline" onClick={handleClear}>
+          {/* Clear */}
+          <RxCross2 />
+        </Button>
       </div>
     </div>
   );
